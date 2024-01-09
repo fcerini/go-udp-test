80e391392d0df5728701c6feb06a18c720adb9c1
Fernando Cerini <fcerini@gmail.com>
2024-01-09 12:26:29 -0300

    config

---
+++ server/config.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+)
+
+// AppConfig Global Configuration
+type AppConfig struct {
+	Puerto           int
+	CantidadPaquetes int
+	Delay            int
+}
+
+// Load config
+func (c *AppConfig) Load() {
+
+	jsonFile, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer jsonFile.Close()
+	byteValue, _ := io.ReadAll(jsonFile)
+	err2 := json.Unmarshal([]byte(byteValue), &c)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+}
